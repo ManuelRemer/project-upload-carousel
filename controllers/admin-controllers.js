380a207566741526d1b7d548755646b75e4a7048
Manuel Remer <manuelremer@web.de>
2022-03-21 12:11:34 +0100

    deleteAdmin functionality test

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -18,14 +18,21 @@ const createAdmin = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ name: admin.name, token });
 };
 
-const loginAdmin = async (req, res) => {
-  const admin = await Admin.findOne(req.body.name);
-  res.status(StatusCodes.OK).json({ name: admin.name, token });
-};
-
 const deleteAdmin = async (req, res) => {
   const admin = await Admin.findOne(req.body.name);
-  res.send({ admin });
+  const { email, password } = req.body;
+  const passwordIsCorrect = await admin.comparePassword(password);
+  console.log(passwordIsCorrect);
+  if (passwordIsCorrect) {
+    const admin = await Admin.findOneAndDelete({ email });
+    res.send({ admin });
+  } else res.send("Wrong password");
+};
+
+const loginAdmin = async (req, res) => {
+  const admin = await Admin.findOne(req.body.name);
+  const token = admin.createJWT();
+  res.status(StatusCodes.OK).json({ name: admin.name, token });
 };
 
 module.exports = { checkIfAdmin, createAdmin, deleteAdmin, loginAdmin };
