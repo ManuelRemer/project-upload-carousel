57870da35719b3f905e34e506b5f6562a7195031
Manuel Remer <manuelremer@web.de>
2022-03-21 16:06:31 +0100

    authentication-error

--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,4 @@
 const BadRequestError = require("./bad-request-error");
+const AuthenticationError = require("./authentication-error");
 
-module.exports = { BadRequestError };
+module.exports = { BadRequestError, AuthenticationError };
