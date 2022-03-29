0925205f6c5a4b1243cf64b62de0129f99989bb1
Manuel Remer <manuelremer@web.de>
2022-03-29 12:41:04 +0200

    chrome test

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // custom stuff
 import { useColorThemeContext } from "../hooks/useColorThemeContext";
@@ -9,12 +9,31 @@ const Navigation = () => {
   const [toHome, setToHome] = useState(false);
   // think about storing toHome in local storage to keep state after reload
   const { navColor, changeNavColor } = useColorThemeContext();
+  const color = getComputedStyle(document.documentElement).getPropertyValue(
+    "--navbar"
+  );
 
+  useEffect(() => {
+    document.documentElement.style.setProperty("--navbar", navColor);
+  }, [navColor]);
+
+  console.log(color);
   return (
     <div className="nav">
       <h1
         className="nav__inner"
-        style={!toHome ? { color: `${navColor}` } : { color: `hotpink` }} // receives a variable whose value is specified by the user
+        style={
+          !toHome
+            ? {
+                // color: navColor,
+                transition: "color 0.2s ease-in-out 0s",
+                color: "var(--navbar)",
+              }
+            : {
+                color: `hotpink`,
+                transition: "color 0.2s ease-in-out 0s",
+              }
+        } // receives a variable whose value is specified by the user
         onClick={() => {
           setToHome(!toHome);
           toHome && changeNavColor(1);
