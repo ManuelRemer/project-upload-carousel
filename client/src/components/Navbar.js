54c40e9fe6854f8e4ad5692fac998969c565b1f7
Manuel Remer <manuelremer@web.de>
2022-04-01 18:29:56 +0200

    nav bug destroied

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,42 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 // custom stuff
 import { useColorThemeContext } from "../hooks/useColorThemeContext";
 // styles
 import "./Navbar.css";
 
 const Navigation = () => {
-  const [toHome, setToHome] = useState(false);
-  // think about storing toHome in local storage to keep state after reload
-  const { navColor, dispatch, linkToHome, changeNaveColor } =
-    useColorThemeContext();
+  const location = useLocation();
+  const { navColor, linkToHome, dispatch } = useColorThemeContext();
+
+  useEffect(() => {
+    location.pathname === "/"
+      ? dispatch({ type: "CHANGE_COLOR_CAROUSEL", payload: "lightgreen" })
+      : dispatch({ type: "CHANGE_COLOR_INFO", payload: "hotpink" });
+  }, [dispatch, location]);
+
+  useEffect(() => {
+    const element = document.querySelector("h1");
+    document.documentElement.style.setProperty("--nav", navColor);
+    element.style.color = navColor;
+  }, [navColor]);
+
+  const NavbarContent = () => {
+    return <h1 className="nav__inner">Anne Franke</h1>;
+  };
 
   return (
     <div className="nav">
-      <h1
-        className="nav__inner"
-        style={
-          !toHome
-            ? {
-                // color: navColor,
-                transition: "color 0.2s ease-in-out 0s",
-                WebkitTransition: "color 0.2s ease-in-out 0s",
-                color: `var(--navbar)`,
-              }
-            : {
-                color: `var(--navbar)`,
-                transition: "color 0.2s ease-in-out 0s",
-                WebkitTransition: "color 0.2s ease-in-out 0s",
-              }
-        } // receives a variable whose value is specified by the user
-        onClick={() => {
-          changeNaveColor(1);
-        }}
-      >
-        {!linkToHome && <Link to="/info">Anne Franke</Link>}
-        {linkToHome && <Link to="/">Anne Franke</Link>}
-      </h1>
+      {!linkToHome && (
+        <Link to="/info">
+          <NavbarContent />
+        </Link>
+      )}
+      {linkToHome && (
+        <Link to="/">
+          <NavbarContent />
+        </Link>
+      )}
     </div>
   );
 };
